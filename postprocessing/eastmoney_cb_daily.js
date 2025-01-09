7284608bdc3d1894a99a1607cc16b8f66ae33e88
Xu Li <xu.li@nyu.edu>
2025-01-09 14:33:26 +0800

    updates

--- postprocessing/eastmoney_cb_daily.js
+++ postprocessing/eastmoney_cb_daily.js
@@ -4,6 +4,11 @@ import { writeJSON, removeFile, readTXT } from 'https://deno.land/x/flat@0.0.14/
 
 const REGEX_JQUERY = /^jQuery\((.*?)\);$/
 
+// 转债代码，转债代码，转债名称, 正股简称，正股代码，正股价，转股价，转股价值，债现价，转股溢价率，信用评级，发行规模
+// "113070", "113070.SH", "渝水转债", "重庆水务", "601158", 4.64, 4.98, 93.3735, 109, 32.81, "AAA", 19
+const ATTRIBUTES = ['SECURITY_CODE', 'SECUCODE', 'SECURITY_NAME_ABBR', 'SECURITY_SHORT_NAME', 'CONVERT_STOCK_CODE',
+    'CONVERT_STOCK_PRICE', 'TRANSFER_PRICE', 'TRANSFER_VALUE', 'CURRENT_BOND_PRICE', 'TRANSFER_PREMIUM_RATIO', 'RATING', 'ACTUAL_ISSUE_SCALE']
+
 const today = new Date().toISOString().split('T')[0]
 
 // Step 1: Create YYYY-mm-dd directory
@@ -19,6 +24,18 @@ let content = await readTXT(filename)
 content = content.replace(REGEX_JQUERY, '$1')
 try {
     content = JSON.parse(content)
+
+    if (content.result && content.result.data) {
+        for (let i = 0; i < content.result.data.length; i++) {
+            const item = content.result.data[i]
+            const obj = {}
+            for (let j = 0; j < ATTRIBUTES.length; j++) {
+                obj[ATTRIBUTES[j]] = item[j]
+            }
+            content.result.data[i] = obj
+        }
+    }
+
     await writeJSON(`data/${today}/${filename}.json`, content)
 } catch (e) {
     console.log(e)
